aec4b316baa07402b24c4b2eecb9d4730bef96b0
Zaid Basharat <143819566+Zaid-Codsoft@users.noreply.github.com>
2024-12-09 02:52:05 +0500

    first commit

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,169 @@
+// Categories Used In Entire App
+export const categories = [
+    "Electronics",
+    "Mobiles",
+    "Laptops",
+    "Fashion",
+    "Appliances",
+    "Home",
+];
+
+// Product Sliders Offers
+export const offerProducts = [
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Office Study Tables",
+        offer: "From Bazaar3,290",
+        tag: "Made In Pakistan Brands",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Mobiles & Accessories",
+        offer: "In Focus Now",
+        tag: "Buy Now!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Calculators",
+        offer: "Min 70% Off",
+        tag: "Buy Now!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Office Study Chairs",
+        offer: "Min 40% Off",
+        tag: "Grab Now!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Smart Watches",
+        offer: "Min 40% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Tripods",
+        offer: "Min 50% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Tripods",
+        offer: "Min 50% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Tripods",
+        offer: "Min 50% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://discountstore.pk/cdn/shop/articles/5_Essential_Mobile_Accessories_for_Tech_Enthusiasts.jpg?v=1723476742",
+        name: "Tripods",
+        offer: "Min 50% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://www.shopright.pk/images/product_gallery/1674315533_1.jpg",
+        name: "Kitchen",
+        offer: "Upto 30% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/k5zn9u80/induction-cook-top/r/q/z/philips-phlps-2100-watt-induction-cooktop-black-original-imaf95d6gupffzr9.jpeg",
+        name: "Philips Inductions",
+        offer: "Upto 55% Off",
+        tag: "Top Rated",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/k7jdg280/lehenga-choli/m/h/f/free-na-102l10171-saara-original-imafprdmenpgbx2n.jpeg",
+        name: "Lehenga Cholis",
+        offer: "Min 60% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/ky3b0y80/television/s/k/h/-original-imagaedxhbam3wmx.jpeg",
+        name: "8K UHD | QLED TVs",
+        offer: "Upto 65% Off",
+        tag: "Discover Now!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/jlgwmfk0/coffee-table/c/h/9/rosewood-sheesham-inlct23-induscraft-honey-brown-original-imaf8kc3gprrchxd.jpeg",
+        name: "Coffee Tables",
+        offer: "From Bazaar1,900",
+        tag: "Relax & Work",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/ksdjma80/headphone/a/u/p/airdopes-131-airdopes-138-boat-original-imag5yz9sfz9bzq8.jpeg",
+        name: "Audio & Video",
+        offer: "Min 50% Off",
+        tag: "Explore Now!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/kbgu1e80/refrigerator-new/g/g/r/steel-inverter-technology-whirlpool-original-imafssu7ghxg5zgh.jpeg",
+        name: "Refrigerators",
+        offer: "Upto 20% Off",
+        tag: "Shop Now!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/kqidx8w0/television/o/h/9/43fa0a00-oneplus-original-imag4gy9perzvrdv.jpeg",
+        name: "Most Trending TVs",
+        offer: "Upto 60% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/keaaavk0/computer/x/m/y/lenovo-na-laptop-original-imafuzt8r5jqppfn.jpeg",
+        name: "Laptops",
+        offer: "Min 20% Off",
+        tag: "Great Savings!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/jtvtz0w0/sling-bag/d/u/k/ponam110-ponam-green-deniza-sling-bag-roseberries-original-imafyuvty2kzymr7.jpeg",
+        name: "Handbags",
+        offer: "Min 70% Off",
+        tag: "Grab Now!",
+    },
+    {
+        image: "https://rukminim1.flixcart.com/image/150/150/jjkbhjk0/sandwich-maker/5/q/g/nova-2-slice-sandwich-maker-original-imaf74a8zrqzfgug.jpeg",
+        name: "Sandwich Makers",
+        offer: "Upto 80% Off",
+        tag: "Buy Now!",
+    }
+]
